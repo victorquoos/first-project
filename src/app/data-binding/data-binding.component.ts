869c8ed328bae6dc6c28867411e699caa3aade7a
Victor Quoos <117126925+victorquoos@users.noreply.github.com>
2023-05-15 20:32:54 -0300

    Propriety Binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
   nome: String = 'Fulano';
+  url: String =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png';
+  esconderTexto: Boolean = true;
 
   getValor() {
     return 1;
