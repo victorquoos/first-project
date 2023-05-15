8537512d9c32a507ea5b05e5180da824265cd69b
Victor Quoos <117126925+victorquoos@users.noreply.github.com>
2023-05-15 20:47:52 -0300

    Event Binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -6,6 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-binding.component.scss'],
 })
 export class DataBindingComponent implements OnInit {
+  valorDigitado: String = '';
+  isMouseOver: Boolean = false;
+
+  clique() {
+    alert('ação do botão');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorDigitado = (<HTMLInputElement>evento.target).value;
+  }
+
+  onMouseOver() {
+    this.isMouseOver = true;
+  }
+
+  onMouseOut() {
+    this.isMouseOver = false;
+  }
+
   nome: String = 'Fulano';
   url: String =
     'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png';
